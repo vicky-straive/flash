923f370b29a3b3a77c64b69f92a975d2ccb1633b
vignesh.muralimohan <vignesh.muralimohan@straive.com>
2024-10-04 17:17:47 +0530

    meta change

--- config/site.ts
+++ config/site.ts
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "FLASH",
+  description: "A beautiful experience.",
   navItems: [
     // {
     //   label: "Home",
